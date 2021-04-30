490c5ff1b5dda909729d06d50aaf3fc44512d48b
jacopo-kyso <jacopo@kyso.io>
2021-04-30 11:52:48 +0200

    grid cards

--- sections/coolspotsCityList.js
+++ sections/coolspotsCityList.js
@@ -13,6 +13,15 @@ const useStyles = makeStyles((theme) => ({
   cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
+    display: "grid",
+  gridTemplateAreas: 
+    "header header header",
+    "nav article ads",
+  gridTemplateRows: "60px 1fr 60px",
+  gridTemplateColumns: "20% 1fr 15%",
+  gridGap: "10px",
+  height: '100vh',
+  margin: 0,
   },
   root: {
     maxWidth: 345,
