f00cb3be831b1f3fbeb12faab06a7947976bf1c8
jacopo-kyso <jacopo@kyso.io>
2021-05-03 11:03:03 +0200

    build errors

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,8 @@ const about = () => {
         <title>About</title>
       </Head>
       <h1>About Page</h1>
+      <p>The first ever OaaS, office as a service☕️ 👩🏻‍💻</p>
+      <p>Coffeeshops, co-working spaces, your fellow remoters... in one place! <br/> Join Coolspots to get access to different locations all around the world. Working remotely doesn't mean isolation!</p>
     </div>
   );
 };
--- pages/cities/valencia.js
+++ pages/cities/valencia.js
@@ -10,6 +10,8 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import "../styles/Home.module.css";
+
 
 const useStyles = makeStyles((theme) => ({
   icon: {
